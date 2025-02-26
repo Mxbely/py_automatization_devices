0bc1abc978ca153d8f99b6a8911f6b8aa59af84d
Max <mxbely98@gmail.com>
2025-02-26 16:22:06 +0200

    add multiprocessing to readme and add docstrings

--- README.md
+++ README.md
@@ -7,12 +7,41 @@ This repository contains Python-based automation tools designed to manage and co
 
 To use this repository, clone it to your local machine and then:
 
+Run script using Docker
+```bash
+docker-compose up --build
+```
+
+Run script unusing Docker
 ```bash
 poetry install
-
-#For run logger
 poetry run python logging/do_it_yourself.py
+```
 
-#For run tests
+For run tests need to use:
+```bash
+poetry install
 poetry run pytest
+```
+
+## Multiprocessing
+Also I tried implement multiprocessing, for working with big files
+Context main.py:
+```bash
+import multiprocessing
+
+manager = multiprocessing.Manager()
+devices = manager.dict()
+broken_devices = manager.list()
+
+with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+    pool.starmap(process_line, [(line, broken_devices, devices) for line in parse_log_file()])
+```
+
+Additional function
+```bash
+def process_line(line: str, broken_devices: list, devices: dict) -> None:
+    check_device_state(line, broken_devices)
+    line_split = line.split(";")
+    device_counter(line_split, devices)
 ```
--- logging/do_it_yourself.py
+++ logging/do_it_yourself.py
@@ -1,4 +1,4 @@
-# import multiprocessing
+import time
 from typing import Generator
 
 FILE_PATH = "logging/app_2.log"
@@ -11,6 +11,15 @@ ERRORS = {
 
 
 def parse_log_file() -> Generator[str, None, None]:
+    """
+    Parses the log file specified by FILE_PATH and yields the last word
+    of each line containing the word "BIG". The single quotes in the 
+    line are removed before splitting.
+    
+    Yields:
+        str: The last word of each relevant line in the log file.
+    """
+
     return (
         row.replace("'", "").split()[-1]
         for row in open(FILE_PATH)
@@ -19,11 +28,20 @@ def parse_log_file() -> Generator[str, None, None]:
 
 
 def check_device_state(line: str, broken_devices: list) -> None:
+    """
+    Checks the state of a device from a log line and appends it to the broken_devices list if the device is broken.
+    The device is considered broken if the line contains the substring "DD".
+    """
+
     if "DD" in line:
         broken_devices.append(line)
 
 
 def print_errors(id: str, errors: list) -> None:
+    """
+    Prints a specific error message for a device id if the error list contains a 1.
+    Otherwise, it prints an unknown device error message.
+    """
     if "1" in errors:
         for index, number in enumerate(errors):
             if number == "1":
@@ -33,6 +51,11 @@ def print_errors(id: str, errors: list) -> None:
 
 
 def detailed_info_by_broken_diveces(broken_devices: list) -> None:
+    """
+    Prints detailed information about broken devices.
+    
+    For each broken device, it prints the id and a specific error message based on the error bits in the device's log line.
+    """
     print()
     for device in broken_devices:
         error_list = []
@@ -53,6 +76,9 @@ def detailed_info_by_broken_diveces(broken_devices: list) -> None:
 
 
 def device_counter(line: str, devices: dict) -> None:
+    """
+    Counts the number of times a device has a successful message and updates the state if the device becomes broken.
+    """
     if line[2] not in devices:
         devices[line[2]] = (
             {"state": line[-2], "count": 0}
@@ -70,25 +96,15 @@ def device_counter(line: str, devices: dict) -> None:
                 devices[line[2]]["count"] += 1
 
 
-# ========For multiprocessing========
-def process_line(line: str, broken_devices: list, devices: dict) -> None:
-    check_device_state(line, broken_devices)
-    line_split = line.split(";")
-    device_counter(line_split, devices)
-
-
 def main() -> None:
-    # ========For multiprocessing========
-    # For some reason this isn't working.
-    # But i tried to implement it.
-
-    # manager = multiprocessing.Manager()
-    # devices = manager.dict()
-    # broken_devices = manager.list()
-
-    # with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
-    #     pool.starmap(process_line, [(line, broken_devices, devices) for line in parse_log_file()])
-    # ===================================
+    """
+    Reads the log file and prints the following information:
+    - The number of all big devices
+    - The number of successful big devices
+    - The number of failed big devices
+    - The number of success messages for each big device
+    - And the detailed information for each broken device
+    """
     devices = {}
     broken_devices = []
 
@@ -113,3 +129,4 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+    time.sleep(1)  # Just for correct output in docker container
